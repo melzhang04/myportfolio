0e92fa253a5cef3427a58eb90f997518ec22e888
Melisa Zhang <101750750+melzhang04@users.noreply.github.com>
2023-01-09 12:06:07 -0500

    Basic Bio created

---
+++ components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1> Welcome to my site! </h1>
+        <br>
+        <p> My name is Melisa Zhang and I'm a current freshman
+        at Columbia University.
+        </p>
+        <img src="/grad1.JPG" alt =" a picture of Melisa at HS graduation" width="250px"/>
+    </div>
+</template>
+
